795134e3d2f8d1876e589d6b42d1709e70257a99
Matheus Frauches <frauches.27@gmail.com>
2020-06-07 01:51:26 -0300

    Apply dropzone image uploader on CreatePoint form

--- src/pages/CreatePoint/index.tsx
+++ src/pages/CreatePoint/index.tsx
@@ -1,5 +1,11 @@
 import axios from "axios";
-import React, { ChangeEvent, useEffect, useState, useCallback, FormEvent } from "react";
+import React, {
+  ChangeEvent,
+  useEffect,
+  useState,
+  useCallback,
+  FormEvent,
+} from "react";
 import { FiArrowLeft } from "react-icons/fi";
 import { Map, Marker, TileLayer } from "react-leaflet";
 import { Link, useHistory } from "react-router-dom";
@@ -7,6 +13,7 @@ import logo from "../../assets/logo.svg";
 import api from "./../../services/api";
 import { LeafletMouseEvent } from "leaflet";
 import "./styles.css";
+import Dropzone from "./../../components/Dropzone/index";
 
 interface Item {
   image_url: string;
@@ -28,6 +35,7 @@ const CreatePoint: React.FC = () => {
   const [items, setItems] = useState<Item[]>([]);
   const [ufs, setUfs] = useState<string[]>([]);
   const [cities, setCities] = useState<string[]>([]);
+  const [selectedFile, setSelectedFile] = useState<File>();
   const [selectedUf, setSelectedUf] = useState("0");
   const [selectedCity, setSelectedCity] = useState("0");
   const [selectedItems, setSelectedItems] = useState<number[]>([]);
@@ -41,8 +49,8 @@ const CreatePoint: React.FC = () => {
   >([0, 0]);
   const [initialMapPosition, setInitialMapPosition] = useState<
     [number, number]
-    >([0, 0]);
-  
+  >([0, 0]);
+
   const history = useHistory();
 
   const handleMapClick = useCallback((event: LeafletMouseEvent) => {
@@ -58,43 +66,53 @@ const CreatePoint: React.FC = () => {
     [formData]
   );
 
-  const handleSelectItem = useCallback((id: number) => {
-    const alreadySelected = selectedItems.findIndex((item) => item === id);
+  const handleSelectItem = useCallback(
+    (id: number) => {
+      const alreadySelected = selectedItems.findIndex((item) => item === id);
 
-    if (alreadySelected >= 0) {
-      const filteredItems = selectedItems.filter((item) => item !== id);
-      setSelectedItems(filteredItems);
-    } else {
-      setSelectedItems([...selectedItems, id]);
-    }
-  }, [selectedItems]);
+      if (alreadySelected >= 0) {
+        const filteredItems = selectedItems.filter((item) => item !== id);
+        setSelectedItems(filteredItems);
+      } else {
+        setSelectedItems([...selectedItems, id]);
+      }
+    },
+    [selectedItems]
+  );
 
-  const handleSubmit = useCallback(async (event: FormEvent) => {
-    event.preventDefault();
+  const handleSubmit = useCallback(
+    async (event: FormEvent) => {
+      event.preventDefault();
 
-    const { name, email, whatsapp } = formData;
-    const uf = selectedUf;
-    const city = selectedCity;
-    const [latitude, longitude] = selectedMapPosition;
-    const items = selectedItems;
+      const { name, email, whatsapp } = formData;
+      const uf = selectedUf;
+      const city = selectedCity;
+      const [latitude, longitude] = selectedMapPosition;
+      const items = selectedItems;
 
-    const data = {
-      name,
-      email,
-      whatsapp,
-      uf,
-      city,
-      latitude,
-      longitude,
-      items
-    }
+      const data = new FormData();
 
-    api.post('/points', data).then(() => {
-      alert('Ponto de coleta criado');
-    });
+      data.append("name", name);
+      data.append("email", email);
+      data.append("whatsapp", whatsapp);
+      data.append("uf", uf);
+      data.append("city", city);
+      data.append("latitude", String(latitude));
+      data.append("longitude", String(longitude));
+      data.append("items", items.join(","));
 
-    history.push('/');
-  }, [formData, history, selectedCity, selectedItems, selectedMapPosition, selectedUf]);
+      if (selectedFile) {
+        data.append("image", selectedFile);
+      }
+
+      api.post("/points", data).then(() => {
+        alert("Ponto de coleta criado");
+      });
+
+      history.push("/");
+    },
+    [formData, history, selectedCity, selectedFile, selectedItems, selectedMapPosition, selectedUf]
+  );
 
   useEffect(() => {
     api.get("/items").then((response) => setItems(response.data));
@@ -150,6 +168,8 @@ const CreatePoint: React.FC = () => {
           Cadastro do <br />
           ponto de coleta
         </h1>
+
+        <Dropzone onFileUploaded={setSelectedFile} />
 
         <fieldset>
           <legend>
